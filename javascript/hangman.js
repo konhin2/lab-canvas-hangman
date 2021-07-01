2509396272459bfd966d2c4c69b4ff45b80467c6
Konhin CM <maskmesta@gmail.com>
2021-06-30 19:27:00 -0500

    iteration1

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,35 +1,108 @@
 class Hangman {
   constructor(words) {
-    this.words = words;
-    // ... your code goes here
+    this.words = words
+    this.secretWord = this.pickWord()
+    this.letters = []
+    this.guessedLetters = ''
+    this.errorsLeft = 10
   }
 
   pickWord() {
-    // ... your code goes here
+    this.secretWord = this.words[Math.floor(Math.random() * this.words.length)]
+    return this.secretWord
   }
 
   checkIfLetter(keyCode) {
-    // ... your code goes here
+    if (keyCode === 81) {
+      return true
+    } else if (keyCode === 87) {
+      return true
+    } else if (keyCode === 69) {
+      return true
+    } else if (keyCode === 82) {
+      return true
+    } else if (keyCode === 84) {
+      return true
+    } else if (keyCode === 89) {
+      return true
+    } else if (keyCode === 85) {
+      return true
+    } else if (keyCode === 73) {
+      return true
+    } else if (keyCode === 79) {
+      return true
+    } else if (keyCode === 80) {
+      return true
+    } else if (keyCode === 65) {
+      return true
+    } else if (keyCode === 83) {
+      return true
+    } else if (keyCode === 68) {
+      return true
+    } else if (keyCode === 70) {
+      return true
+    } else if (keyCode === 71) {
+      return true
+    } else if (keyCode === 72) {
+      return true
+    } else if (keyCode === 74) {
+      return true
+    } else if (keyCode === 75) {
+      return true
+    } else if (keyCode === 76) {
+      return true
+    } else if (keyCode === 186) {
+      return true
+    } else if (keyCode === 90) {
+      return true
+    } else if (keyCode === 88) {
+      return true
+    } else if (keyCode === 67) {
+      return true
+    } else if (keyCode === 86) {
+      return true
+    } else if (keyCode === 66) {
+      return true
+    } else if (keyCode === 78) {
+      return true
+    } else if (keyCode === 77) {
+      return true
+    } else {
+      return false
+    }
   }
 
   checkClickedLetters(letter) {
-    // ... your code goes here
+    if (this.letters.includes(letter)) {
+      return false
+    } else {
+      return true
+    }
   }
 
   addCorrectLetter(letter) {
-    // ... your code goes here
+    this.guessedLetters += letter
   }
 
   addWrongLetter(letter) {
-    // ... your code goes here
+    this.letters.push(letter)
+    this.errorsLeft--
   }
 
   checkGameOver() {
-    // ... your code goes here
+    if (this.errorsLeft > 0) {
+      return false
+    } else {
+      return true
+    }
   }
 
   checkWinner() {
-    // ... your code goes here
+    if(this.guessedLetters.length === this.secretWord.length){
+      return true
+    }else{
+      return false
+    }
   }
 }
 
